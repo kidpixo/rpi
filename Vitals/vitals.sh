5fdfa3e8d98809e2618bbaf4cc29abef2d3b6edf
daguy666 <jpistonejr@gmail.com>
2013-08-17 10:31:41 +0000

    Added echo between lines for formatting

--- Vitals/vitals.sh
+++ Vitals/vitals.sh
@@ -26,36 +26,51 @@ volts=measure_volts
 echo
 echo "Current System Status"
 sleep 1
+echo
 echo "Who is logged in?"
 sleep 1
+echo
 w
 sleep 1
+echo
 echo "Cpu Temp:"
 sleep 1
+echo
 $vcgencmd measure_temp
 sleep 1
+echo
 echo "System Uptime:"
 sleep 1
+echo
 uptime
 sleep 1
+echo
 echo "Firmware Version:"
 sleep 1
+echo
 $vcgencmd version
 sleep 1
+echo
 echo "Show Voltages:"
 sleep 1
+echo
 echo "Core Volts:"
 $vcgencmd $volts core
 sleep 1
+echo
 echo "Sdram_c"
 $vcgencmd $volts sdram_c
 sleep 1
+echo
 echo "Sdram_i"
 $vcgencmd $volts sdram_i
 sleep 1
+echo
 echo "Sdram_p"
 $vcgencmd $volts sdram_p
 sleep 1
+echo
 echo "CPU Info:"
 sleep 1
+echo
 cat /proc/cpuinfo
