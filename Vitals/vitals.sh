5a73201867360c6c7b91c6ad3dec7ec13a89a68e
kidpixo <kidpixo@gmail.com>
2013-11-27 14:59:14 +0100

    Cosmetic surgery
    - deleted sleep anr/or dramatic effects
    - deleted empy echo between line for cmpact output

--- Vitals/vitals.sh
+++ Vitals/vitals.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#This will get you some system info on your Raspberry Pi 
+#This will get you some system info on your Raspberry Pi
 #Built using Raspbian Wheezy
-#sleep for dramatic effect
-#
+# originally from http://thekillingtime.com/?p=22
+# forked by github.com/kidpixo from github.com/daguy666/rpi
+
 read -p "Would you like to get System Stats? (Press Y or N) " -n 1
 echo
 if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -18,59 +19,27 @@ echo
    echo "| Raspbian for the raspberry pi.|"
    echo "_________________________________"
 
-
-
-vcgencmd=/opt/vc/bin/vcgencmd 
+vcgencmd=/usr/bin/vcgencmd
 volts=measure_volts
 
 echo
 echo "Current System Status"
-sleep 1
-echo
 echo "Who is logged in?"
-sleep 1
-echo
 w
-sleep 1
-echo
 echo "Cpu Temp:"
-sleep 1
-echo
 $vcgencmd measure_temp
-sleep 1
-echo
 echo "System Uptime:"
-sleep 1
-echo
 uptime
-sleep 1
-echo
 echo "Firmware Version:"
-sleep 1
-echo
 $vcgencmd version
-sleep 1
-echo
 echo "Show Voltages:"
-sleep 1
-echo
 echo "Core Volts:"
 $vcgencmd $volts core
-sleep 1
-echo
 echo "Sdram_c"
 $vcgencmd $volts sdram_c
-sleep 1
-echo
 echo "Sdram_i"
 $vcgencmd $volts sdram_i
-sleep 1
-echo
 echo "Sdram_p"
 $vcgencmd $volts sdram_p
-sleep 1
-echo
 echo "CPU Info:"
-sleep 1
-echo
 cat /proc/cpuinfo
